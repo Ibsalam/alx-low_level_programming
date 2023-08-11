4b9c112ec9b3c03d4fbd0ecc317c73e440a28110
ibsalam <https://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@github.com/ibsalam/alx-system_engineering-devops.git>
2023-08-11 13:24:44 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - Programme that print a line using printf
+ * Return: 0 (done)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+		return (0);
