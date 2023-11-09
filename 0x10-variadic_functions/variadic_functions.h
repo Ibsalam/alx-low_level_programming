5c0b51d366068369c7d85ed9f12391048be723c5
ibsalam <asio4edu@gmail.com>
2023-11-09 05:12:40 -0800

    returns the sum of all its parameters

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,10 @@
+#ifndef _PUTCHAR_VARIADIC
+#define _PUTCHAR_VARIADIC
+
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
