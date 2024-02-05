e74567bfd7b2ba44c0e40b2d360a53de2d2a02ec
ibsalam <asio4edu@gmail.com>
2024-02-05 03:48:19 -0800

    hastable updated

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -9,9 +9,5 @@
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-    unsigned long int hash_value;
-
-    hash_value = hash_djb2(key);
-
     return (hash_value % size);
 }
